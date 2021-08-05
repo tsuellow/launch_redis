bd9f1907eea7de6dfae12ab33873dced9114b4f6
tsuellow <theo.suellow@gmail.com>
2021-08-05 12:19:57 -0600

    f commit

--- install_redis.sh
+++ install_redis.sh
@@ -8,5 +8,5 @@ sudo chmod -R 777 /etc/redis
 sudo chmod -R 777 /var/log
 sudo chmod -R 777 /var/redis/6379
 sudo cp /etc/redis/redis.conf /etc/redis/6379.conf
-sudo sed -e "s/^daemonize no$/daemonize yes/" -e "s/^bind 127.0.0.1$/bind 172.31.91.38/" -e "s/^dir \.\//dir \/var\/redis\/6379\//" -e "s/^loglevel verbose$/loglevel notice/" -e "s/^logfile \"\"$/logfile \/var\/log\/redis.log/" /etc/redis/6379.conf | sudo tee /etc/redis/6379.conf
+sudo sed -e "s/^daemonize no$/daemonize yes/" -e "s/^bind 127.0.0.1$/bind 172.31.93.48/" -e "s/^dir \.\//dir \/var\/redis\/6379\//" -e "s/^loglevel verbose$/loglevel notice/" -e "s/^logfile \"\"$/logfile \/var\/log\/redis.log/" /etc/redis/6379.conf | sudo tee /etc/redis/6379.conf
 redis-server 6379.conf
